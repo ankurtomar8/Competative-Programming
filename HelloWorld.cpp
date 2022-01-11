ea06ae9775a1c14cdab3c3345092501ed740113c
AnkurDev <ankurtomar8@outlook.com>
2022-01-11 07:47:18 +0530

    Start testing Env and Build System

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string s ;
+	 cin>>s;
+	 cout<<"Hello to  World Competative Programming "<<s<<endl;
+	// while(true); infinite loop testing for build
+}
